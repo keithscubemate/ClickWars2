7b30174c9a1fb6b3f7340579131ed951e936b284
Austin Jones <austin.jones@uster.com>
2024-04-11 11:13:14 -0400

    making repo

---
+++ ClickWars2.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClickWars2.Data;
+using ClickWars2.ViewModel;
+
+namespace ClickWars2.Cli;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        var dataProv = new GameDataProvider();
+        var gamesViewModel = new GamesViewModel(dataProv);
+        gamesViewModel.LoadAsync().Wait();
+
+        foreach (var game in gamesViewModel.Games)  
+        {
+            Console.WriteLine($"{game.PlayerName}:{game.Id}");
+        }
+    }
+}
